0f8fb0b44a4ea0a7176031899889e8a880f2e1f0
Istvan Szukacs <istvan@streambrightdata.com>
2017-09-27 20:18:59 +0200

    Updating demo2

--- src/demo2.ml
+++ src/demo2.ml
@@ -1,4 +1,47 @@
-(* Not all OCaml is functional, with ref and set of operators you have access to mutable variables *)
+(* *  Imperative features
+   *
+   *  Most of OCaml works without mutable variables but it is possible to
+   *  use imperative features like mutable variables when needed.
+   *
+   *    Mutable variable
+   *
+   *      !       to fetch the current value
+   *      :=      assign new value
+   *
+   *      example: a := !a + 1
+   *
+   *
+   * *)
+
+(* *
+   *  Files, modules and interfaces
+   *
+   *      Opening modules can be global or local withing a let scope
+   *
+   *      ```open Str``` imports all the Str interface into the current module.
+   *      Files are mapped to module names eg. file_name -> Module_name , external modules
+   *      can be added by importing the library at compile time. Accessing module functions
+   *      is always possible with the fully qualified name, eg: Random.float 0.0 if the library
+   *      was present at build time (not sure about dynamic linking) or it is part of core OCaml.
+   *
+   *      Error: Unbound module Core
+   *
+   *      src/pi.ml -> Pi
+   *
+   *      src/pi.mli, interface for module Pi
+   *
+   *      example interface for this file:
+   *
+   *            type point
+   *            val pi_known : float
+   *            val origin : point
+   *            val distance_between_points : point -> point -> float
+   *            val distance_from_origin : point -> float
+   *            val find_pi : number_of_samples:int -> float
+   *
+   *
+   *  *)
+
 
 module R = Random
 
@@ -21,8 +64,7 @@ let find_pi ~number_of_samples:s =
     let xr = R.float 1.0 in
     let yr = R.float 1.0 in
     let p = {x=xr; y=yr} in
-    if distance_from_origin p < 1.0 then
-      within := !within + 1
+    if distance_from_origin p < 1.0 then within := !within + 1
   done;
   ((float_of_int !within) /. (float_of_int s)) *. 4.0
 
---
+++ src/demo2.mli
@@ -0,0 +1,6 @@
+type point
+val pi_known : float
+val origin : point
+val distance_between_points : point -> point -> float
+val distance_from_origin : point -> float
+val find_pi : number_of_samples:int -> float
