e00624092f18d20c84bd52c94fa11c6a13bf9508
Istvan Szukacs <istvan@streambrightdata.com>
2017-09-30 21:13:00 +0200

    Adding WebGL

--- gl/webpack.config.js
+++ gl/webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    async: './lib/js/src/async/app.js',
-    flow: './lib/js/src/flow/flow.js'
+    app: './lib/js/src/gl/app.js'
   },
   output: {
     path: path.join(__dirname, "bundledOutputs"),
